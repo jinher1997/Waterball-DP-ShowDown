47f376162e2858d642bb99d4627bfad913da37eb
PeterChen <jinher1997@gmail.com>
2022-06-19 19:18:11 +0800

    feat : All feature of  Deck .

--- src/Deck.cs
+++ src/Deck.cs
@@ -2,15 +2,35 @@ namespace ShowDown
 {
     public class Deck
     {
-        protected Card[] cards;
-        public void Shuffle()
+        protected Stack<Card> cards = new Stack<Card>();
+        public Deck()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    cards.Push(new Card((Rank)j, (Suit)i));
+                }
+            }
         }
 
-        public Card Draw()
+        public void Shuffle()
         {
-            throw new NotImplementedException();
+            Random rnd = new Random();
+            var newCards = cards.ToList();
+            for (int i = 0; i < newCards.Count; i++)
+            {
+                int j = rnd.Next(newCards.Count);
+                Card temp = newCards[i];
+                newCards[i] = newCards[j];
+                newCards[j] = temp;
+            }
+            cards = new Stack<Card>(newCards);
+        }
+
+        public Card DrawCard()
+        {
+            return cards.Pop();
         }
 
     }
