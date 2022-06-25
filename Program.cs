6d54d9d12a97d28221c0913c0105ea40cdea66b5
PeterChen <jinher1997@gmail.com>
2022-06-25 14:45:05 +0800

    refactor : makeup some codes.

--- Program.cs
+++ Program.cs
@@ -4,12 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Player[] players = new Player[4];
-            for (int i = 0; i < 3; i++)
-            {
-                players[i] = new HumanPlayer();
-            }
-            players[3] = new AIPlayer();
+            Player[] players = new Player[4] { new HumanPlayer(), new HumanPlayer(), new HumanPlayer(), new AIPlayer() };
             var game = new ShowDown(players);
             game.Start();
         }
--- src/Round.cs
+++ src/Round.cs
@@ -6,20 +6,32 @@ namespace ShowDown
         public void Execute(Player[] players)
         {
             Card[] cards = new Card[4];
+            RequestPlayersDecideWhetherToExchangeHands(players);
+            RequestPlayersShowCard(players, cards);
+            DetermineThisRoundWinner(players, cards);
+        }
+
+        void RequestPlayersDecideWhetherToExchangeHands(Player[] players)
+        {
             for (int i = 0; i < players.Length; i++)
             {
                 Player currentPlayer = players[i];
                 var otherPlayers = players.Where(player => player != currentPlayer).ToArray();
                 currentPlayer.TakeTurnToDecideWhetherToExchangeHands(otherPlayers);
             }
+        }
 
+        void RequestPlayersShowCard(Player[] players, Card[] cards)
+        {
             for (int playerIndex = 0; playerIndex < players.Length; playerIndex++)
             {
                 Player currentPlayer = players[playerIndex];
                 cards[playerIndex] = currentPlayer.Show();
             }
+        }
 
-            //Compare all cards and determine the winner
+        void DetermineThisRoundWinner(Player[] players, Card[] cards)
+        {
             int highestCardIndex = 0;
             for (int cardIndex = 1; cardIndex < cards.Length; cardIndex++)
             {
